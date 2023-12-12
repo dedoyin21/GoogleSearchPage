c626accfc9f8835679c3debf9ddd310fa47951a8
deedee213 <133877095+dedoyin21@users.noreply.github.com>
2023-12-12 17:11:09 +0100

    Initial commit

---
+++ class.js
@@ -0,0 +1,28 @@
+/* let result = 0; */
+
+function multiplyTwo(n) {
+    console.log( n * 2 );
+}
+
+multiplyTwo(5)
+
+let numbers = [3,10,5];
+function divide (b){
+    return
+}
+
+
+
+<div>
+        <section class="footer">
+            <div class="lastPart">
+                <ul class="letterList">
+                    <li class="letterG" style="color: rgb(8, 124, 212)">G</li>
+                    <li class="firstO" style="color: rgb(189, 9, 48),">o</li>
+                    <li class="letterO" style="color: rgb(220, 192, 8);">ooooooooo</li>
+                    <li class="letterg" style="color: rgb(8, 124, 212)">g</li>
+                    <li class="letterl" style="color: rgb(70, 145, 5);">l</li>
+                    <li class="lettere" style="color: rgb(189, 9, 48);">e</li>
+                    <li><i class="fa-solid fa-chevron-right" style="color: rgb(8, 124, 212);"></i></li>
+                </ul>
+           
